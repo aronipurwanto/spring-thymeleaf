77fb4ff3728db8417c44c417bdcd793dc5aace2a
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2024-05-12 11:13:47 +0700

    initial commit

---
+++ src/main/java/com/arp/springthymeleaf/SpringThymeleafApp.java
@@ -0,0 +1,13 @@
+package com.arp.springthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringThymeleafApp.class, args);
+    }
+
+}
---
+++ src/test/java/com/arp/springthymeleaf/SpringThymeleafAppTests.java
@@ -0,0 +1,13 @@
+package com.arp.springthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
