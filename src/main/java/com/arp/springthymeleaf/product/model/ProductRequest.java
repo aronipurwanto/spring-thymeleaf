fc169489a3d983b56a4ef06336418de176be23d9
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2024-05-12 15:31:18 +0700

    feat: fix add and get by id

--- src/main/java/com/arp/springthymeleaf/product/model/ProductRequest.java
+++ src/main/java/com/arp/springthymeleaf/product/model/ProductRequest.java
@@ -1,4 +1,17 @@
 package com.arp.springthymeleaf.product.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductRequest {
+    private Long id;
+    private Long categoryId;
+    private String name;
+    private String description;
+    private Double price;
+    private Double quantity;
 }
--- src/main/java/com/arp/springthymeleaf/product/model/ProductResponse.java
+++ src/main/java/com/arp/springthymeleaf/product/model/ProductResponse.java
@@ -1,4 +1,17 @@
 package com.arp.springthymeleaf.product.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductResponse {
+    private Long id;
+    private Long categoryId;
+    private String name;
+    private String description;
+    private Double price;
+    private Double quantity;
 }
